4ecaf6360be1505f39208251413c3ca5d173d609
ovenCode <41307304+ovenCode@users.noreply.github.com>
2024-07-14 01:59:47 +0200

    Added file containing constants used across various files

---
+++ desktop/misc/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktop.misc
+{
+    internal static class Constants
+    {
+        public static string AssetsAdventureImagesPath { get; } = "./assets/files/adventure-pl_pl/pl_pl/data/";
+    }
+}
