4e72fdae28397bbb6eb1b6523097a4614f285cb5
ovenCode <41307304+ovenCode@users.noreply.github.com>
2024-07-14 01:57:49 +0200

    Added function to parse attachments into AugmentObjects

--- desktop/data/Models/AugmentObject.cs
+++ desktop/data/Models/AugmentObject.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using desktop.misc;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +19,24 @@ namespace desktop.data.Models
         public string AugmentName { get; set; } = null!;
         public string AugmentCode { get; set; } = null!;
         public string AugmentText { get; set; } = null!;
+
+        public static bool TryParse(IAttachment attachment, string attachmentCode, out AugmentObject? augment)
+        {
+            try
+            {
+                BitmapSource? source;
+                string attachmentType = attachment.GetType().ToString().ToLower();
+
+                augment = new AugmentObject { AugmentImage = Tools.GetImageSource($"{Constants.AssetsAdventureImagesPath}{attachmentType}/{attachmentCode}.png", out source) ? source : null, AugmentImagePath = $"{Constants.AssetsAdventureImagesPath}{attachmentType}/{attachmentCode}.png", AugmentWidth = 50, AugmentTextWidth = "0", AugmentText = "", AugmentName = "", AugmentCode = attachmentCode };
+                return true;
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+                //throw;
+            }
+            augment = null;
+            return false;
+        }
     }
 }
