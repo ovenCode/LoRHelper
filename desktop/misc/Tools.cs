5b2dbb11e6b5786d33e96a918e337fc20345d4ea
ovenCode <41307304+ovenCode@users.noreply.github.com>
2024-07-14 02:00:32 +0200

    Added Tools class containing generalised reusable functions across the solution

---
+++ desktop/misc/Tools.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace desktop.misc
+{
+    internal static class Tools
+    {
+        public static bool GetImageSource(string path, out ImageSource? imageSource)
+        {
+            if (path.Length == 0)
+            {
+                path = "image-help-1.png"; // change to image-not-found
+            }
+            try
+            {
+                Stream imageStreamSource = new FileStream(
+                    path,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read
+                );
+                PngBitmapDecoder decoder = new PngBitmapDecoder(
+                    imageStreamSource,
+                    BitmapCreateOptions.PreservePixelFormat,
+                    BitmapCacheOption.Default
+                );
+                BitmapSource bitmapSource = decoder.Frames[0];
+                /*BitmapImage image = new BitmapImage();
+                Uri? uri;
+                image.BeginInit();
+                image.UriSource = Uri.TryCreate(path, UriKind.Relative, out uri) ? uri : new Uri(@"");
+                image.EndInit();*/
+
+                imageSource = bitmapSource;
+                //Trace.WriteLine("Succesfully got image source");
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                Trace.WriteLine($"File {path} was not found.");
+                imageSource = null;
+                //logger.LogMessage(error.Message, MessageType.Error, error.GetType().Name).Wait();
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool GetImageSource(string path, out BitmapSource? imageSource)
+        {
+            if (path.Length == 0)
+            {
+                path = "image-help-1.png"; // change to image-not-found
+            }
+            try
+            {
+                Stream imageStreamSource = new FileStream(
+                    path,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read
+                );
+                PngBitmapDecoder decoder = new PngBitmapDecoder(
+                    imageStreamSource,
+                    BitmapCreateOptions.PreservePixelFormat,
+                    BitmapCacheOption.Default
+                );
+                BitmapSource bitmapSource = decoder.Frames[0];
+                /*BitmapImage image = new BitmapImage();
+                Uri? uri;
+                image.BeginInit();
+                image.UriSource = Uri.TryCreate(path, UriKind.Relative, out uri) ? uri : new Uri(@"");
+                image.EndInit();*/
+
+                imageSource = bitmapSource;
+                //Trace.WriteLine("Succesfully got image source");
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                Trace.WriteLine($"File {path} was not found.");
+                imageSource = null;
+                //logger.LogMessage(error.Message, MessageType.Error, error.GetType().Name).Wait();
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
