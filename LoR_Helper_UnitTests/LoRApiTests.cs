4d79ffd00bcce4a3bf76313b94065d9199331df0
ovenCode <41307304+ovenCode@users.noreply.github.com>
2025-06-06 21:36:32 +0200

    Adding UnitTests

---
+++ LoR_Helper_UnitTests/LoRApiTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using LoRAPI.Controllers;
+using Moq;
+using Xunit;
+
+namespace LoR_Helper_UnitTests;
+
+public class LoRApiTests
+{
+    LoRAPI.Models.Deck expectedDeck;
+    Mock<ILoRApiHandler> mock;
+
+    public LoRApiTests()
+    {
+        mock = new Mock<ILoRApiHandler>();
+        expectedDeck = new LoRAPI.Models.Deck();
+        mock.Setup(p => p.GetDeckAsync()).ReturnsAsync(expectedDeck);
+    }
+
+    [Fact]
+    public async Task GetDeckAsync_SuccessfullyReturns()
+    {
+        ILoRApiHandler handler = mock.Object;
+        LoRAPI.Models.Deck deck = await handler.GetDeckAsync();
+
+        Assert.Equal(deck, expectedDeck);
+    }
+}
